20d6b02639d69e3c396ad7f175d0726c888e4a71
redcode9 <33084859+redcode9@users.noreply.github.com>
2025-10-17 01:04:25 +0200

    First commit

---
+++ Image_Uploader/Image_Uploader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Image_Uploader
+//
+//  Created by Piero Carrieri on 17/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Image_Uploader/Image_Uploader/Image_UploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_UploaderApp.swift
+//  Image_Uploader
+//
+//  Created by Piero Carrieri on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Image_UploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
